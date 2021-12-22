7450b008e59f38405ac48fb6153b1fc4b0aa6b6a
KT-VIETNAM\tkv-nguyendinhtuan <nguyendinhtuan@koeitecmo.vn>
2021-12-22 15:22:32 +0700

    + Fucking preload

--- preload.js
+++ preload.js
@@ -1,12 +1,27 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const { contextBridge, ipcRenderer } = require("electron");
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+contextBridge.exposeInMainWorld("api", {
+	send: (channel, data) => {
+		let validChannels = ["toMain"];
+		if (validChannels.includes(channel)) {
+			ipcRenderer.send(channel, data);
+		}
+	},
+	receive: (channel, func) => {
+		let validChannels = ["fromMain"];
+		if (validChannels.includes(channel)) {
+			ipcRenderer.on(channel, (event, ...args) => func(...args));
+		}
+	}
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector)
+		if (element) element.innerText = text
+	}
+
+	for (const type of ['chrome', 'node', 'electron']) {
+		replaceText(`${type}-version`, process.versions[type])
+	}
 })
