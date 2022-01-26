f92b6bd0313cbc40804a86001d455529409b87b4
KT-VIETNAM\tkv-nguyendinhtuan <nguyendinhtuan@koeitecmo.vn>
2022-01-26 10:25:07 +0700

    + Meow

--- main.js
+++ main.js
@@ -1,39 +1,63 @@
-const { app, BrowserWindow, shell, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const audio = require("win-audio").speaker;
+
+let mainWindow;
 
 function createWindow () {
-	const mainWindow = new BrowserWindow({
-		width: 350,
-		height: 500,
+	mainWindow = new BrowserWindow({
+		width: 128,
+		height: 128,
 		// width: 800,
 		// height: 600,
+		x: 40,
+		y: 200,
 		webPreferences: {
 			nodeIntegration: false,
 			contextIsolation: true,
 			enableRemoteModule: false,
-			preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, 'preload.js'),
 		},
 		frame: false,
-		transparent: true
+		transparent: true,
+		minimizable: false,
+		alwaysOnTop: true
 	})
 
 	mainWindow.loadFile('index.html')
 	// mainWindow.webContents.openDevTools();
+
+	mainWindow.on("hide", (event) => {
+		event.preventDefault();
+	})
+
+	mainWindow.on("minimize", (event) => {
+		event.preventDefault();
+	})
+
+	setupAudio();
 }
 
 app.whenReady().then(() => {
-	createWindow()
+	createWindow();
 
 	app.on('activate', function () {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow()
-		console.log('asdfasdf');
-	})
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 })
 
 app.on('window-all-closed', function () {
 	if (process.platform !== 'darwin') app.quit()
 })
 
-ipcMain.on("openExternal", (event, args) => {
-	shell.openExternal(args);
-});
+function setupAudio() {
+	audio.polling(100);
+
+	audio.events.on("change", (volume) => {
+		mainWindow.webContents.send("volumeChanged", volume.new);
+	});
+
+	audio.events.on("toggle", (muted) => {
+		mainWindow.webContents.send("mutedToggle", muted.new);
+	});
+}
--- renderer.js
+++ renderer.js
@@ -1,13 +1,10 @@
-let appPaths = {
-	"Effekseer": "file:///D:/Effekseer161e/Tool/Effekseer.exe",
-	"Godot": "file:///D:/Godot_v3.4-stable_win64.exe",
-	"Unity Hub": `file:///C:/Program Files/Unity Hub/Unity Hub.exe`,
-	"GIMP": "file:///C:/Program Files/GIMP 2/bin/gimp-2.10.exe"
-}
+let cat = document.getElementById("cat");
 
-let apps = document.getElementsByClassName("app");
-for (let i = 0; i < apps.length; i++) {
-	apps[i].onclick = function() {
-		window.api.send("openExternal", appPaths[apps[i].getAttribute("data-app")]);
-	}
-}
+window.api.receive("volumeChanged", (volume) => {
+    console.log("volumeChanged");
+    console.log(volume);
+})
+
+window.api.receive("mutedToggle", (isMuted) => {
+    cat.setAttribute("src", "images/" + (isMuted ? "muted" : "unmuted") + ".png");
+})
