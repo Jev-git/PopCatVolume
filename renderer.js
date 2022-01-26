40baf684d5b3fe64fa29255c9bd56d347838444a
KT-VIETNAM\tkv-nguyendinhtuan <nguyendinhtuan@koeitecmo.vn>
2022-01-26 12:03:36 +0700

    + Fried

--- renderer.js
+++ renderer.js
@@ -3,61 +3,76 @@ let cat = document.getElementById("cat");
 let mouth = document.getElementById("mouth");
 
 const MAX_MOUTH_SIZE = 35;
-const MIN_VOLUME_MOUTH_DISPLAY = 20;
-const MAX_VOLUME_MOUTH_DISPLAY = 60;
+const PAUSE_THRESHOLD = 20;
+const POP_THRESHOLD = 60;
+const FRIED_THRESHOLD = 80;
 
 window.api.receive("init", (isMuted, volume) => {
+    setCatImage(isMuted, volume);
+    setMouthVisibility(isMuted, volume);
     if (isMuted) {
-        setMutedCatImage(true);
-        setMouthVisibility(false);
         setCatShake(false);
     } else {
-        setMutedCatImage(volume < MIN_VOLUME_MOUTH_DISPLAY);
-        setMouthVisibility(volume >= MIN_VOLUME_MOUTH_DISPLAY);
         setMouthSize(volume);
-        setCatShake(volume >= MAX_VOLUME_MOUTH_DISPLAY);
+        setCatShake(volume >= POP_THRESHOLD);
     }
 });
 
 window.api.receive("volumeChanged", (volume) => {
-    setMutedCatImage(volume < MIN_VOLUME_MOUTH_DISPLAY);
-    setMouthVisibility(volume >= MIN_VOLUME_MOUTH_DISPLAY);
-    if (volume < MIN_VOLUME_MOUTH_DISPLAY) {
+    setCatImage(false, volume);
+    setMouthVisibility(false, volume);
+    if (volume < PAUSE_THRESHOLD) {
         setCatShake(false);
     } else {
         setMouthSize(volume);
-        setCatShake(volume >= MAX_VOLUME_MOUTH_DISPLAY);
+        setCatShake(volume >= POP_THRESHOLD);
     }
 });
 
 window.api.receive("muteToggled", (isMuted, volume) => {
+    setCatImage(isMuted, volume);
+    setMouthVisibility(isMuted, volume);
     if (isMuted) {
-        setMutedCatImage(true);
-        setMouthVisibility(false);
         setCatShake(false);
     } else {
-        setMutedCatImage(volume < MIN_VOLUME_MOUTH_DISPLAY);
-        setMouthVisibility(volume >= MIN_VOLUME_MOUTH_DISPLAY);
-        if (volume < MIN_VOLUME_MOUTH_DISPLAY) {
+        if (volume < PAUSE_THRESHOLD) {
             setCatShake(false);
         } else {
             setMouthSize(volume);
-            setCatShake(volume >= MAX_VOLUME_MOUTH_DISPLAY);
+            setCatShake(volume >= POP_THRESHOLD);
         }
     }
 });
 
-function setMutedCatImage(isMuted) {
-    cat.setAttribute("src", "images/" + (isMuted ? "muted" : "unmuted") + ".png");
+function setCatImage(isMuted, volume) {
+    if (isMuted) {
+        cat.setAttribute("src", "images/pause.png");
+    } else {
+        if (volume < PAUSE_THRESHOLD) {
+            cat.setAttribute("src", "images/pause.png");
+        } else if (volume < FRIED_THRESHOLD) {
+            cat.setAttribute("src", "images/pop.png");
+        } else {
+            cat.setAttribute("src", "images/fried.png");
+        }
+    }
 }
 
 function setMouthSize(volume) {
-    let clampedVolume = Math.min(Math.max(volume, MIN_VOLUME_MOUTH_DISPLAY), MAX_VOLUME_MOUTH_DISPLAY);
-    mouth.style.width = clampedVolume / MAX_VOLUME_MOUTH_DISPLAY * MAX_MOUTH_SIZE + "px";
+    let clampedVolume = Math.min(Math.max(volume, PAUSE_THRESHOLD), POP_THRESHOLD);
+    mouth.style.width = clampedVolume / POP_THRESHOLD * MAX_MOUTH_SIZE + "px";
 }
 
-function setMouthVisibility(isVisible) {
-    mouth.style.visibility = isVisible ? "visible" : "hidden";
+function setMouthVisibility(isMuted, volume) {
+    if (isMuted) {
+        mouth.style.visibility = "hidden";
+    } else {
+        if (volume >= PAUSE_THRESHOLD && volume < FRIED_THRESHOLD) {
+            mouth.style.visibility = "visible";
+        } else {
+            mouth.style.visibility = "hidden";
+        }
+    }
 }
 
 function setCatShake(isShaking) {
